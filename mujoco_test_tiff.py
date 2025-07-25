73ae139d0f36d0c093c11c1abb93d647aab4f475
Wonhee Park <tottenhamsonny7@naver.com>
2025-07-25 21:01:06 +0900

    nnn

--- mujoco_test_tiff.py
+++ mujoco_test_tiff.py
@@ -1,5 +1,6 @@
 import os, math, rasterio, numpy as np, mujoco, glfw
 from mujoco import viewer
+import time
 
 
 TIF_PATH = "flydrone/fly_drone/envs/image_cut.tif"
@@ -22,10 +23,10 @@ nrow, ncol = dem.shape
 print(f"DEM ready : {nrow}×{ncol}")
 
 
-SX = SY = 5 #이게 5키로였나? 근데 5000으로 하면 걍 평평해보임
-SZ = 1.2              
+SX = SY = 5000 #이게 5키로였나? 근데 5000으로 하면 걍 평평해보임
+SZ = 1200             
 
-xml = f"""
+'''xml = f"""
 <mujoco>
   <asset>
     <hfield name="terrain" nrow="{nrow}" ncol="{ncol}"
@@ -36,17 +37,26 @@ xml = f"""
     <geom type="hfield" hfield="terrain"
           pos="0 0 0"
           rgba="0.6 0.5 0.4 1"/>
-    <camera name="angled_view" pos="0.073 -10.819 10.398" xyaxes="1.000 -0.000 0.000 0.000 0.707 0.707"/>
+    <camera name="view" pos="491.262 -11127.187 9767.584" xyaxes="1.000 0.029 -0.000 -0.020 0.709 0.705"/>
   </worldbody>
 </mujoco>
-""" 
+""" '''
+xml = "flydrone/fly_drone/envs/skydio_x2/scene.xml"
 
-model = mujoco.MjModel.from_xml_string(xml)
+model = mujoco.MjModel.from_xml_path(xml)
 adr = model.hfield_adr[0]
 model.hfield_data[adr: adr + nrow*ncol] = dem.flatten()
 data = mujoco.MjData(model)     
 
 
 with viewer.launch_passive(model, data) as v:
+    start = time.time()
     while v.is_running():
+        step_start = time.time()
         mujoco.mj_step(model, data)
+        with v.lock():
+          v.opt.flags[mujoco.mjtVisFlag.mjVIS_CONTACTPOINT] = int(data.time % 2)
+        v.sync()
+        time_until_next_step = model.opt.timestep - (time.time() - step_start)
+        if time_until_next_step > 0:
+          time.sleep(time_until_next_step)
